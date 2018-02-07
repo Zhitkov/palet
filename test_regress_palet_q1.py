e62941e389cdc7a1d66a21cf08a30ba8d5059404
ovosol40 <33435087+ovosol40@users.noreply.github.com>
2018-02-07 21:08:51 +0300

    Update test_regress_palet_q1.py

--- test_regress_palet_q1.py
+++ test_regress_palet_q1.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1251 -*-
 import pytest
 from palet import calculator
 
@@ -9,9 +10,24 @@ from palet import calculator
     (0,13),
     (23,-23),
     (-24,-25),
-    (123, 0.45)
+    (123, 0.45),
     ])
     
 def test_regress_out__case_0001(how_much, size):
-    result = calculator(how_much, size)
-    return
+        result = calculator(how_much, size)
+        assert 0 != size and how_much
+
+
+#def test_regress_out__case_0002(how_much, size):
+ #       try:
+  #             result = how_much / size
+   #     except ZeroDivisionError:
+    #            print 'На ноль делить нельзя'
+	
+def test_regress_out__case_0003(how_much, size):
+        result = calculator(how_much, size)
+        assert type(result) != type(size) and type(how_much)
+
+def test_regress_out__case_0004(how_much):
+        assert isinstance(how_much, float)
+        assert isinstance(size, float)
